8b3914f94040b92111dd278f492913f5c7110988
lmlb-astro <136018556+lmlb-astro@users.noreply.github.com>
2024-03-26 20:23:23 -0700

    update to make sure each sentence ends with a .

--- check_text.py
+++ check_text.py
@@ -1,5 +1,11 @@
 #This is an example text. It shows that the program works. It verifies whether there are consecutive repeating sentences. This program checks wether a sentence repeats a previous sentence. This is a test.
 
+# PCCW 's chief operating officer , Mike Butcher , and Alex Arena , the chief financial officer , will report directly to Mr So . Current Chief Operating Officer Mike Butcher and Group Chief Financial Officer Alex Arena will report to So .
+
+# According to the federal Centers for Disease Control and Prevention ( news - web sites ) , there were 19 reported cases of measles in the United States in 2002 . The Centers for Disease Control and Prevention said there were 19 reported cases of measles in the United States in 2002 .
+
+
+import numpy as np
 from transformers import AutoModel, AutoTokenizer
 
 class repetition_identifier:
@@ -17,11 +23,16 @@ class repetition_identifier:
             sent1, sent2 = sentences[idx], sentences[idx+1]
 
             ## create tokenized data input for the model
+            print(sent1)
             inps = self.tokenizer(sent1, sent2, return_tensors = "pt")
+            #inps2 = self.tokenizer(sent2, return_tensors = "pt")
+            #if idx == 0: print(inps1)
     
             ## evaluate the input
             output = self.id_model(**inps)
-            #print(output)
+
+            ## use the output to determine whether the sentence repeat the same thing
+            print(np.argmax(output, axis = -1)) # DOES THIS WORK?
     
     
     ## Obtains the input text, splits it into sentences and then calls the function to find the repeating sentences
@@ -29,16 +40,23 @@ class repetition_identifier:
         # get text from terminal and split into sentences
         text = input("Please input the message:")
         sentences = self.__text_to_sentences(text)
-        print(sentences)
     
-        rep_inds = self.__inds_repetitive_sentences(sentences) # CONTINUE HERE
+        rep_inds = self.__inds_repetitive_sentences(sentences)
+        # CONTINUE HERE
     
         ## store the indices and the sentences
     
     
     ## basic implementation to split the sentence
     def __text_to_sentences(self, text):
-        return text.split(".")
+        ## split the text into sentences
+        sentences = text.split(".")[:-1]
+
+        ## make sure every sentence ends with a "."
+        for idx in range(0, len(sentences)):
+            sentences[idx] = "{sentence}.".format(sentence = sentences[idx])
+
+        return sentences
     
     
     ## retuns the phrases that repeat the same content
